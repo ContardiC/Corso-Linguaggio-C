3f306472f928d1f3cb9fa6839d6c3d63f0a2ffeb
Carlo Contardi <carlocontardi@MBP-di-Carlo.fritz.box>
2024-09-21 15:56:48 +0200

    update input.c

--- Lezioni/Lezione1/input.c
+++ Lezioni/Lezione1/input.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main(){
     int numero_intero;
-    double numero_decimale;
+    float numero_decimale;
+    double numero_decimale_doppia_precisione;
     char carattere;
     printf("Inserisci un numero intero \n");
     scanf("%d",&numero_intero);
-    printf("Inserisci un numero decimale\n");
-    scanf("%lf",&numero_decimale);
+    printf("Inserisci un numero decimale (lettura con f) \n");
+    scanf("%f",&numero_decimale);
+    printf("Inserisci un numero decimale (lettura con lf) \n");
+    scanf("%lf",&numero_decimale_doppia_precisione);
     printf("Inserisci un carattere\n");
     fflush(stdin);
     scanf("%c",&carattere);
-    printf("Hai inserito %d e %.2lf  e %c\n", numero_intero,numero_decimale, carattere);
+    printf("Hai inserito %d e %.2f e %.2lf  e %c\n", numero_intero,numero_decimale, numero_decimale_doppia_precisione, carattere);
+
 }
